f214686f5c40dd3c6629cb10bcf96c9358f250bb
Sarika B <sbudhira@us.ibm.com>
2021-03-19 02:48:48 -0400

    Manage Playbook

---
+++ docs/component---src-pages-404-js-b5708e2407fe5c58f53a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{w2l6:function(e,t,s){"use strict";s.r(t);s("q1tI");var n=s("Wjw8"),r=s("qKvR"),i=[{href:"/design-system/start-designing",text:"Start designing"},{href:"/design-system/start-developing",text:"Start developing"},{href:"/team/designers",text:"Team"},{href:"/resources",text:"Resources"}];t.default=function(){return Object(r.b)(n.a,{links:i})}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b5708e2407fe5c58f53a.js.map
