32f2e83132bbcdbd58fbd0eea8dd1072cb88b2d1
Sarika B <sbudhira@us.ibm.com>
2021-07-12 01:22:49 -0400

    Manage Playbook

---
+++ docs/5f6bb6fb-909e5d042a25bc267dbb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{Jp0J:function(e,t,n){"use strict";n.d(t,"a",(function(){return o}));var r,c=n("iVHE"),a=n("q1tI"),i=n.n(a),w=["children"],o=i.a.forwardRef((function(e,t){var n=e.children,a=Object(c.b)(e,w);return i.a.createElement(c.a,Object(c.c)({width:20,height:20,viewBox:"0 0 32 32",xmlns:"http://www.w3.org/2000/svg",fill:"currentColor",ref:t},a),r||(r=i.a.createElement("path",{d:"M16 14L6 24 7.4 25.4 16 16.8 24.6 25.4 26 24zM4 8H28V10H4z"})),n)}))}}]);
+//# sourceMappingURL=5f6bb6fb-909e5d042a25bc267dbb.js.map
