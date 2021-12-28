780f73b2b4ff33a737f0b497aca43c63f51204b9
Sarika B <sbudhira@us.ibm.com>
2021-12-28 12:45:26 -0500

    Manage Playbook

--- src/gatsby-theme-carbon/components/Switcher/Switcher.js
+++
@@ -1,7 +0,0 @@
-/* DNA-Comment: Gatsby shadow component for carbon gatsby theme. TODO - Remove 9-point icon. */
-import React from "react"
-import Switcher from 'gatsby-theme-carbon/src/components/Switcher/Switcher';
-
-const CustomSwitcher = props => <Switcher {...props} />;
-
-export default CustomSwitcher;
